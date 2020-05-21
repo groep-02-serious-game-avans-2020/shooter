480a903d9d4cc1f386a0cc4e6380247fcc7ffaaf
Erik van Dam <erik.vandam@trivago.com>
2020-05-21 21:16:50 +0200

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public Text countText;
+    public Text winText;
+    public PlayerController playerController;
+
+    private int count = 0;
+
+	void Start()
+    {
+        playerController.OnCollisionEvent += IncreaseScore;
+    }
+
+    void Update()
+    {
+
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	float moveVertical = Input.GetAxis("Vertical");
+
+    	playerController.Move(moveHorizontal, moveVertical);
+    }
+
+    public void IncreaseScore()
+    {
+    	count = count + 1;
+
+        SetCountText();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+    public delegate void OnCollision();
+    public event OnCollision OnCollisionEvent;
+
+	public float speed;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+	void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(float moveHorizontal, float moveVertical)
+    {
+        this.moveHorizontal = moveHorizontal;
+        this.moveVertical = moveVertical;
+    }
+
+    void FixedUpdate()
+    {
+    	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+    	rb.AddForce(movement * speed);
+    }
+}
