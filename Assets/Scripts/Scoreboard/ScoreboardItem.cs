9cf100046065d84a9f9dcf080bd576f20b937421
DoCNL <sabok@avans.nl>
2020-06-08 14:14:31 +0200

    added scoreboard

---
+++ Assets/Scripts/Scoreboard/ScoreboardItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardItem
+{
+
+
+    public string userName { get; set; }
+    public int position { get; set; }
+    public int score { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"name\": \"{userName}\", \"score\": {score}";
+        }
+    
+}
