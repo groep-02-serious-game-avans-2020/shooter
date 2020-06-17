aac263c6021318d9ff401e34aedb82726773cd3e
DoCNL <sabok@avans.nl>
2020-06-17 02:30:29 +0200

    Added character customization

---
+++ Assets/Scripts/ColorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorController : MonoBehaviour
+{
+
+    public Material material;
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().color = material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Material SelectColor()
+    {
+        return material;
+    }
+}
